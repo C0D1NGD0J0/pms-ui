11f1e95ba7ecc3e174b873040652ed9730637f2f
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2025-11-18 06:52:38 -0500

    refactor: remove lease-related components and their associated logic
    - Deleted CoTenantsTab, DocumentsTab, FinancialDetailsTab, LeaseTermsTab, PropertySelectionTab, SignatureTab, TenantInfoTab, and their index export.
    - Removed the LeasesPage component and its logic.

--- src/app/(protectedRoutes)/leases/[cuid]/new/components/tabs/index.ts
+++
@@ -1,8 +0,0 @@
-export { PropertySelectionTab } from "./PropertySelectionTab";
-export { TenantInfoTab } from "./TenantInfoTab";
-export { LeaseTermsTab } from "./LeaseTermsTab";
-export { FinancialDetailsTab } from "./FinancialDetailsTab";
-export { SignatureTab } from "./SignatureTab";
-export { AdditionalTermsTab } from "./AdditionalTermsTab";
-export { CoTenantsTab } from "./CoTenantsTab";
-export { DocumentsTab } from "./DocumentsTab";
